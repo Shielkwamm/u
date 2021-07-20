59121f765d5416d8ba7d544a9d80fa8fe8fd4dd1
Neobii <iwilldevelopcodeforyou@gmail.com>
2021-07-20 05:02:12 -0500

    a tiny bit more u;⒰

--- src/pages/u.tsx
+++ src/pages/u.tsx
@@ -9,14 +9,19 @@ const U = () => {
   return (
     <Layout>
       <Scrolling/>
+      <hr/>
       <h1>ṻ</h1>
       <Typography>ṵṷṹṻụủứừ⒰υṳ</Typography>
+      <hr/>
       <h1>ự</h1>
       <Typography>ỤỦỨỪỬῢΰῦṺṴUὺ</Typography>
+      <hr/>
       <h1>Ṹ</h1>
       <Typography>ῧuṲỮỰṶṸύῠῡ</Typography>
+      <hr/>
       <h1>ὺ</h1>
       <Typography>ửữựὐὑὒὓὔὕὖὗ</Typography>
+      <hr/>
       <Scrolling/>
     </Layout>
   );
