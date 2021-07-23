ef0485d5a98b04f88760f2943711beda926151d3
Neobii <iwilldevelopcodeforyou@gmail.com>
2021-07-23 06:16:36 -0500

    listing productsu u

--- src/components/product/listSingle.tsx
+++ src/components/product/listSingle.tsx
@@ -3,44 +3,27 @@ import { gql, request } from 'graphql-request';
 
 const ListSingle = ({ product }) => (
   <>
-    <h2>{product.productName}</h2>
+    <h2>{product.name}{product.u}</h2>
+    {product.description ? (<p><b>description</b>: {product.description}</p>): null}
+    {product.bigIdea ? ( <p><em>bigIdea</em>: {product.bigIdea}</p> ): null }
     <Grid container spacing={3}>
-      
       <Grid item xs={4}>
-        <a href={`https://github.com/${product.productOwner}`}>Product Owner</a></Grid>
+        <a href={`https://github.com/${product.productOwner}`}>Product Owner: {product.productOwner}</a></Grid>
       <Grid item xs={4}>
         <a href={`${product.repo}`}>repo</a>
       </Grid>
       <Grid item xs={4}>
         <a href={`https://github.com/${product.name}`}>github</a>
       </Grid>
-      
     </Grid>
-    </>
+    {product.lean? (<h4>lean</h4>):null}
+    {product.lean?.map((ps, index) => (
+      <div key={index} >
+        <h4><em>problem</em>: {ps.problem}</h4>
+        <p><b>solution</b>: {ps.solution}</p>
+      </div>
+    ))}
+  </>
 )
 
 export default ListSingle;
-
-const queryOrganization = gql `query ($organizationName: String!){ 
-  organization(login: $organizationName) { 
-    name
-    memberStatuses(first:1) {
-      edges {
-        node {
-        	user {
-            login
-          }
-        }
-      }
-    }
-    teams(first: 20) {
-      totalCount
-      edges {
-        node {
-          name
-          description
-        }
-      }
-    }
-  }
-}`
--- src/pages/products.tsx
+++ src/pages/products.tsx
@@ -3,9 +3,15 @@ import Layout from "~/components/product/layout";
 import ListSingle from "~/components/product/listSingle";
 import { useSpring, animated } from 'react-spring'
 import Button from '@material-ui/core/Button';
+import useSWR from 'swr'
+
+const fetcher = url => fetch(url).then(res => res.json());
+  
 
 const ProductsList = () => {
-  const products = useProducts();
+  //const organizationName = router.query.login;
+  const { data, error } = useSWR(`https://raw.githubusercontent.com/staticStatic/u-data/main/products.json`, fetcher);
+  //const products = useProducts();
   const skewX = useSpring({
     loop: true,
     to: [
@@ -15,10 +21,11 @@ const ProductsList = () => {
     from: { skewX: 0.5 },
     config: { duration: 700 },
   })
+  
   return (
     <Layout>
       <h2><a style={{display: "inline-block"}} href="/products"><animated.div style={skewX}>prod<Button variant="contained">ṵ</Button>cts</animated.div></a>  </h2>
-      {products?.map((product) => (
+      {data?.map((product) => (
         <ListSingle key={product._id} product={product}/>
       ))}
     </Layout>
